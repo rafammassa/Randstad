24720c3b7ffa2928ee8ee3b56fa6cea53d3b81e6
rafammassa <rafaella_massa@hotmail.com>
2024-09-16 13:38:22 -0300

    Criando a classe Pessoa (nome, idade, apresentar())

---
+++ exemplofundamentos/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemplofundamentos.Models
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+
+        public void Apresentar(){
+            Console.WriteLine($"Olá, meu nome é {nome}, e tenho {idade} anos.")
+        }
+    }
+}
