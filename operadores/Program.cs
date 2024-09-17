54f7f5c26a555c483f609aff068a7dccd82bb959
rafammassa <rafaella_massa@hotmail.com>
2024-09-17 10:46:35 -0300

    Convertendo variável

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -4,4 +4,16 @@ int b = 20;
 
 int c = a + b;
 
+//c = c + 5;
+c += 5;
+
+//c = c - 6;
+c -= 6;
 Console.WriteLine(c);
+
+//Cast - Casting
+int d = Convert.ToInt32("5");
+Console.WriteLine(d);
+
+int e = int.Parse("6");
+Console.WriteLine(e);
--- operadores/obj/Debug/net8.0/operadores.AssemblyInfo.cs
+++ operadores/obj/Debug/net8.0/operadores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("operadores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c6ecb6abf00cdadc5d46bf83ba1de4cc189200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af79fa2704b5f6805ff64405cf84ed97b2105616")]
 [assembly: System.Reflection.AssemblyProductAttribute("operadores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("operadores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
