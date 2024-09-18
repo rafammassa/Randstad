c4858f5d4fef1885dbedb75516ce71573c99df8a
rafammassa <rafaella_massa@hotmail.com>
2024-09-18 16:13:40 -0300

    Construindo menu interativo

--- estruturasDeRepeticao/Program.cs
+++ estruturasDeRepeticao/Program.cs
@@ -19,14 +19,57 @@ while (c <=10)
    }
 }
 
-int soma = 0, numero3 = 0;
+//int soma = 0, numero3 = 0;
  
-do
+//do
+//{
+//    Console.WriteLine("Digite um número: ");
+//    numero3 = Convert.ToInt32(Console.ReadLine());
+
+//    soma += numero3;
+//} while (numero3 != 0);
+
+//Console.WriteLine("A soma dos números digitados é: " + soma);
+
+string opcao;
+bool exibirMenu = true;
+
+while (exibirMenu)
 {
-    Console.WriteLine("Digite um número: ");
-    numero3 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar Cliente");
+    Console.WriteLine("2 - Buscar Cliente");
+    Console.WriteLine("3 - Apagar Cliente");
+    Console.WriteLine("4 - Encerrar");
 
-    soma += numero3;
-} while (numero3 != 0);
+    opcao = Console.ReadLine();
 
-Console.WriteLine("A soma dos números digitados é: " + soma);
+    Console.Clear();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            Environment.Exit(0);
+            break;
+    }
+}
+
+Console.WriteLine("O programa foi encerrado");
--- estruturasDeRepeticao/obj/Debug/net8.0/estruturasDeRepeticao.AssemblyInfo.cs
+++ estruturasDeRepeticao/obj/Debug/net8.0/estruturasDeRepeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f8d36087b0d34534dd08e7b7537100b956aa7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45df1669c32956847202b4de252c64aeb330c99a")]
 [assembly: System.Reflection.AssemblyProductAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
