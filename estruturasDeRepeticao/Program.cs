27f8d36087b0d34534dd08e7b7537100b956aa7f
rafammassa <rafaella_massa@hotmail.com>
2024-09-18 15:29:45 -0300

    Laço de repetição WHILE

--- estruturasDeRepeticao/Program.cs
+++ estruturasDeRepeticao/Program.cs
@@ -4,3 +4,12 @@ for (int i = 0; i <= 10; i++)
 {
     Console.WriteLine($"{numero} x {i} = {numero * i}");
 }
+
+int numero2 = 8;
+int c = 0;
+
+while (c <=10)
+{
+   Console.WriteLine($"{numero2} x {c} = {numero2 * c}"); 
+   c++;
+}
--- estruturasDeRepeticao/obj/Debug/net8.0/estruturasDeRepeticao.AssemblyInfo.cs
+++ estruturasDeRepeticao/obj/Debug/net8.0/estruturasDeRepeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cebee59bb94c5d4964295dc31bd0da174980b63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21383374bf8873e086387efd5b071ff225b1a1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estruturasDeRepeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
