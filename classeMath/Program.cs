8671974721b57f4dfb8f4784bf7129527b4175d2
rafammassa <rafaella_massa@hotmail.com>
2024-09-18 10:28:21 -0300

    Seno, coseno e tangente com classe Math

--- classeMath/Program.cs
+++ classeMath/Program.cs
@@ -5,3 +5,6 @@ calc.Subtrair(10,50);
 calc.Multiplicar(15, 45);
 calc.Dividir(2, 2);
 calc.Potencia(2, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
--- classeMath/obj/Debug/net8.0/classeMath.AssemblyInfo.cs
+++ classeMath/obj/Debug/net8.0/classeMath.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classeMath")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a27e4de80b71c0e0c2df849fdb9bb44870558a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad4591b633d66480dcd9303b0323f0cd42cb572")]
 [assembly: System.Reflection.AssemblyProductAttribute("classeMath")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classeMath")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
