02229d29d4e6dffcf84d0fb27a4914fef73edd58
rafammassa <rafaella_massa@hotmail.com>
2024-09-23 09:16:21 -0300

    Primeira aula sobre Arrays

---
+++ exemplofundamentos/arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arrayInteiros = new int[3];
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição n°{i} - {arrayInteiros[i]}");
+}
