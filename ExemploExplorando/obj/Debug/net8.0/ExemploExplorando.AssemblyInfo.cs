6c5eb5ac985f490289442a7dbc2b954e708f4502
rafammassa <rafaella_massa@hotmail.com>
2024-10-02 15:18:42 -0300

    Implementando método ObterQuantidadeDeAlunosMatriculados (return int)

--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9ee6c568c2cd65e66060bc2100725b7647e31f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51003cfb9585ed8b7c3b9a477885446875efe53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
