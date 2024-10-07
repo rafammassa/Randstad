c29b60346ede724824dec7002300ab38aa9660b9
rafammassa <rafaella_massa@hotmail.com>
2024-10-07 09:43:00 -0300

    Utilizando classe Stack (pilha)

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ExemploExplorando.Models;
+using System.Collections;
 using System.Globalization;
 
 Pessoa p1 = new Pessoa();
@@ -96,6 +97,26 @@ Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
 fila.Enqueue(10);
 
 foreach (var item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Stack<int> pilha = new Stack<int>();
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (var item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+pilha.Push(20);
+
+foreach (var item in pilha)
 {
     Console.WriteLine(item);
 }
