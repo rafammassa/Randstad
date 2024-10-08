bf253d1df24d8dfbde6adb9cd3fc10b4b0cae043
rafammassa <rafaella_massa@hotmail.com>
2024-10-08 14:10:37 -0300

    Escrevendo um arquivo JSON

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -200,4 +200,6 @@ Venda v1 = new Venda(1, "Material de escritório", 25.00M);
 
 string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
 
+File.WriteAllText("Arquivos/vendas.json", serializado);
+
 Console.WriteLine(serializado);
--- ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cabe366f0a53bb97c9aacd3cdf5f71863e4446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ba80a2b8adca02a082deef9921d61deead7cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
